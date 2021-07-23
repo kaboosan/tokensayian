857e35afde5accbd528f6402805311449415e4ac
Kamel <bourahla.kamel@gmail.com>
2021-07-23 15:42:50 +0200

    Add kyc contract + test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,15 @@
 var SaiyanToken = artifacts.require("./SaiyanToken.sol");
 var SaiyanTokenSale = artifacts.require("./SaiyanTokenSale.sol");
+var KycContract = artifacts.require("./KycContract.sol");
+
 require('dotenv').config({path: '../.env'});
 
 module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
     await deployer.deploy(SaiyanToken, process.env.INITIAL_TOKENS);
-    await deployer.deploy(SaiyanTokenSale, 1, addr[0], SaiyanToken.address);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(SaiyanTokenSale, 1, addr[0], SaiyanToken.address, KycContract.address)
+
     let tokenInstance = await SaiyanToken.deployed();
     await tokenInstance.transfer(SaiyanTokenSale.address, process.env.INITIAL_TOKENS);
 };
